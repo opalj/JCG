e41e90c9c3f9fc35ae3abec8d0375a1ba24de3be
Michael Eichberg <mail@michael-eichberg.de>
2015-10-07 14:23:43 +0200

    added the code from OPAL for testing call graph algorithms
    Signed-off-by: Michael Eichberg <mail@michael-eichberg.de>

---
+++ src/main/java/callgraph/constructors/DefaultConstructor.java
@@ -0,0 +1,93 @@
+/* BSD 2-Clause License:
+ * Copyright (c) 2009 - 2014
+ * Software Technology Group
+ * Department of Computer Science
+ * Technische Universität Darmstadt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package callgraph.constructors;
+
+import org.opalj.test.annotations.InvokedConstructor;
+
+import callgraph.base.AbstractBase;
+import callgraph.base.AlternateBase;
+import callgraph.base.Base;
+import callgraph.base.ConcreteBase;
+import callgraph.base.SimpleBase;
+
+/**
+ * This class was used to create a class file with some well defined attributes. The
+ * created class is subsequently used by several tests.
+ * 
+ * <b>NOTE</b><br>
+ * This class is not meant to be (automatically) recompiled; it just serves documentation
+ * purposes.
+ * 
+ * <!--
+ * 
+ * 
+ * 
+ * 
+ * INTENTIONALLY LEFT EMPTY TO MAKE SURE THAT THE SPECIFIED LINE NUMBERS ARE STABLE IF THE
+ * CODE (E.G. IMPORTS) CHANGE.
+ * 
+ * 
+ * 
+ * -->
+ * 
+ * @author Marco Jacobasch
+ */
+public class DefaultConstructor {
+
+    @SuppressWarnings("unused")
+    @InvokedConstructor(receiverType = "callgraph/base/SimpleBase", line = 66)
+    public void createSimpleBase() {
+        Base simpleBase = new SimpleBase();
+    }
+
+    class MyBase extends AbstractBase {
+
+        @Override
+        public void abstractMethod() {/* empty */
+        }
+    }
+
+    @SuppressWarnings("unused")
+    @InvokedConstructor(receiverType = "callgraph/constructors/DefaultConstructor$MyBase", line = 79)
+    public void createAbstractBase() {
+        Base abstractBase = new MyBase();
+    }
+
+    @SuppressWarnings("unused")
+    @InvokedConstructor(receiverType = "callgraph/base/ConcreteBase", line = 85)
+    public void createConcreteBase() {
+        Base concreteBase = new ConcreteBase();
+    }
+
+    @SuppressWarnings("unused")
+    @InvokedConstructor(receiverType = "callgraph/base/AlternateBase", line = 91)
+    public void createAlternateBase() {
+        Base alternerateBase = new AlternateBase();
+    }
+}
---
+++ src/main/java/simpleCallgraph/A.java
@@ -0,0 +1,95 @@
+/* BSD 2-Clause License:
+ * Copyright (c) 2009 - 2014
+ * Software Technology Group
+ * Department of Computer Science
+ * Technische Universität Darmstadt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package simpleCallgraph;
+
+import org.opalj.test.annotations.InvokedConstructor;
+import org.opalj.test.annotations.InvokedMethod;
+import org.opalj.test.annotations.InvokedMethods;
+
+/**
+ * This class was used to create a class file with some well defined properties. The
+ * created class is subsequently used by several tests.
+ * 
+ * <b>NOTE</b><br>
+ * This class is not meant to be (automatically) recompiled; it just serves documentation
+ * purposes.
+ * 
+ * <!--
+ * 
+ *
+ * 
+ * INTENTIONALLY LEFT EMPTY (THIS AREA CAN BE EXTENDED/REDUCED TO MAKE SURE THAT THE
+ * SPECIFIED LINE NUMBERS ARE STABLE.
+ * 
+ * 
+ * 
+ * 
+ * -->
+ * 
+ * @author Marco Jacobasch
+ * @author Michael Reif
+ */
+public class A implements Base {
+
+    Base b = new B();
+
+    @Override
+    @InvokedMethod(receiverType = "simpleCallgraph/B", name = "callOnInstanceField", line = 65)
+    public String callOnInstanceField() {
+        return b.callOnInstanceField();
+    }
+
+    @Override
+    @InvokedMethod(receiverType = "simpleCallgraph/B", name = "callOnConstructor", line = 72)
+    @InvokedConstructor(receiverType = "simpleCallgraph/B", line = 72)
+    public void callOnConstructor() {
+        new B().callOnConstructor();
+    }
+
+    @Override
+    @InvokedMethods({
+            @InvokedMethod(receiverType = "simpleCallgraph/A", name = "callOnMethodParameter", line = 82),
+            @InvokedMethod(receiverType = "simpleCallgraph/A", name = "callOnConstructor", line = 83),
+            @InvokedMethod(receiverType = "simpleCallgraph/B", name = "callOnConstructor", line = 83) })
+    public void callOnMethodParameter(Base b) {
+        if (b != null) {
+            this.callOnMethodParameter(null);
+            b.callOnConstructor();
+        }
+    }
+
+    @InvokedMethods({
+            @InvokedMethod(receiverType = "simpleCallgraph/A", name = "callOnConstructor", line = 91),
+            @InvokedMethod(receiverType = "simpleCallgraph/B", name = "callOnConstructor", line = 92) })
+    public void directCallOnConstructor() {
+        new A().callOnConstructor();
+        new B().callOnConstructor();
+    }
+
+}
