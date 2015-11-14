90c4181a059adf2a47b68e0df4ac589dd0030e7c
rkolosovs <roberts.kolosovs@stud.tu-darmstadt.de>
2015-11-14 18:07:04 +0100

    added example for invalid serializable class (contains non-serializable field)

---
+++ src/main/java/callgraph/serializableNonSerializableField/SerializableWithNonSerializableField.java
@@ -0,0 +1,83 @@
+/* BSD 2-Clause License:
+ * Copyright (c) 2009 - 2014
+ * Software Technology Group
+ * Department of Computer Science
+ * Technische Universität Darmstadt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package callgraph.serializableNonSerializableField;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import callgraph.serializableInvalidSuperclass.SerializableSubclass;
+
+/**
+ * This class was used to create a class file with some well defined attributes.
+ * The created class is subsequently used by several tests.
+ * 
+ * Serializable class with a non-serializable field. As writeObject and readObject use the default 
+ * implementation, all serialization and de-serialization attempts result in an error.
+ * 
+ * <b>NOTE</b><br>
+ * This class is not meant to be (automatically) recompiled; it just serves
+ * documentation purposes.
+ * 
+ * <!--
+ * 
+ * 
+ * 
+ * INTENTIONALLY LEFT EMPTY TO MAKE SURE THAT THE SPECIFIED LINE NUMBERS ARE
+ * STABLE IF THE CODE (E.G. IMPORTS) CHANGE.
+ * 
+ * 
+ * 
+ * 
+ * -->
+ * 
+ * @author Roberts Kolosovs
+ */
+public class SerializableWithNonSerializableField implements Serializable {
+
+	public StringBox label; //field with non-serializable type (StringBox)
+	
+	private Object writeReplace(){ //entry point via serialization
+		return new SerializableWithNonSerializableField(); //default implementation
+	}
+	
+	private void writeObject(java.io.ObjectOutputStream out) 
+			throws IOException{ //entry point via serialization;
+		out.defaultWriteObject(); //default implementation; results in java.io.NotSerializableException
+	}
+	
+	private Object readResolve(){ //no entry point; 
+								  //de-serialization attempts result in java.io.WriteAbortedException
+		return new SerializableWithNonSerializableField(); //default implementation
+	}
+	
+	private void readObject(java.io.ObjectInputStream in) 
+			throws ClassNotFoundException, IOException{ //entry point via de-serialization;
+		in.defaultReadObject(); //default implementation; results in java.io.WriteAbortedException
+	}
+}
---
+++ src/main/java/callgraph/serializableNonSerializableField/StringBox.java
@@ -0,0 +1,30 @@
+package callgraph.serializableNonSerializableField;
+
+/**
+ * This class was used to create a class file with some well defined attributes.
+ * The created class is subsequently used by several tests.
+ * 
+ * A simple box wrapping a String object.
+ * 
+ * <b>NOTE</b><br>
+ * This class is not meant to be (automatically) recompiled; it just serves
+ * documentation purposes.
+ * 
+ * <!--
+ * 
+ * 
+ * 
+ * INTENTIONALLY LEFT EMPTY TO MAKE SURE THAT THE SPECIFIED LINE NUMBERS ARE
+ * STABLE IF THE CODE (E.G. IMPORTS) CHANGE.
+ * 
+ * 
+ * 
+ * 
+ * -->
+ * 
+ * @author Roberts Kolosovs
+ */
+public class StringBox {
+
+	public String content; //String wrapped by the class
+}
