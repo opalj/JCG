04273c5480e165c7ee071b44b00d1762bffa8ee0
rkolosovs <roberts.kolosovs@stud.tu-darmstadt.de>
2015-11-15 02:16:45 +0100

    added example for serializable class extending another serializable class with public writeReplace method

---
+++ src/main/java/callgraph/publicWriteReplaceInSuperclass/ExtendsSerializable.java
@@ -0,0 +1,75 @@
+/* BSD 2-Clause License:
+ * Copyright (c) 2009 - 2014
+ * Software Technology Group
+ * Department of Computer Science
+ * Technische Universität Darmstadt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package callgraph.publicWriteReplaceInSuperclass;
+
+import java.io.IOException;
+
+/**
+ * This class was used to create a class file with some well defined attributes.
+ * The created class is subsequently used by several tests.
+ * 
+ * Serializable class extending another serializable class. The superclass implements public 
+ * writeReplace method which is called whenever this class is serialized. Thus (assuming this 
+ * state of code is the only one being run) no serialized instances of this class exist. Whenever 
+ * this happens, writeReplace of the superclass is called twice.
+ * 
+ * <b>NOTE</b><br>
+ * This class is not meant to be (automatically) recompiled; it just serves
+ * documentation purposes.
+ * 
+ * <!--
+ * 
+ * 
+ * 
+ * INTENTIONALLY LEFT EMPTY TO MAKE SURE THAT THE SPECIFIED LINE NUMBERS ARE
+ * STABLE IF THE CODE (E.G. IMPORTS) CHANGE.
+ * 
+ * 
+ * 
+ * 
+ * -->
+ * 
+ * @author Roberts Kolosovs
+ */
+public class ExtendsSerializable extends SerializableWithPublicWriteReplace {
+
+	private static final long serialVersionUID = 5471170331658694098L;
+
+	private void readObject(java.io.ObjectInputStream in) 
+			throws ClassNotFoundException, IOException{ //dead code; 
+														//no serialized instances of this class exist 
+														//(provided the code has always been in this state)
+		in.defaultReadObject(); //default implementation
+	}
+	
+	private void writeObject(java.io.ObjectOutputStream out) 
+			throws IOException{ //dead code; call graph is redirected to superclass by writeReplace during serialization
+		out.defaultWriteObject(); //default implementation
+	}
+}
---
+++ src/main/java/callgraph/publicWriteReplaceInSuperclass/SerializableWithPublicWriteReplace.java
@@ -0,0 +1,90 @@
+/* BSD 2-Clause License:
+ * Copyright (c) 2009 - 2014
+ * Software Technology Group
+ * Department of Computer Science
+ * Technische Universität Darmstadt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package callgraph.publicWriteReplaceInSuperclass;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * This class was used to create a class file with some well defined attributes.
+ * The created class is subsequently used by several tests.
+ * 
+ * Serializable class with public writeReplace method. It returns a new instance of this class with 
+ * label marked as being replaced. This method is called whenever this class or one of its subclasses 
+ * attempts a serialization. If a subclass causes the method to be called, it is called twice.
+ * 
+ * <b>NOTE</b><br>
+ * This class is not meant to be (automatically) recompiled; it just serves
+ * documentation purposes.
+ * 
+ * <!--
+ * 
+ * 
+ * 
+ * INTENTIONALLY LEFT EMPTY TO MAKE SURE THAT THE SPECIFIED LINE NUMBERS ARE
+ * STABLE IF THE CODE (E.G. IMPORTS) CHANGE.
+ * 
+ * 
+ * 
+ * 
+ * -->
+ * 
+ * @author Roberts Kolosovs
+ */
+public class SerializableWithPublicWriteReplace implements Serializable {
+
+	private static final long serialVersionUID = 2368715977184990608L;
+	
+	public String label;
+	
+	public SerializableWithPublicWriteReplace(){} //default no-args constructor
+	
+	public SerializableWithPublicWriteReplace(String arg){ //constructor explicitly setting the label
+		label = arg;
+	}
+
+	public Object writeReplace(){ //entry point via serialization;
+								  //called during the serialization of ExtendsSerializable
+		return new SerializableWithPublicWriteReplace("replaced " + label); //modifies the label to trace the event of replacement object
+	}
+	
+	private void readObject(java.io.ObjectInputStream in) 
+			throws ClassNotFoundException, IOException{ //entry point via de-serialization
+		in.defaultReadObject(); //default implementation
+	}
+	
+	private void writeObject(java.io.ObjectOutputStream out) 
+			throws IOException{ //entry point via serialization
+		out.defaultWriteObject(); //default implementation
+	}
+	
+	private Object readResolve(){ //entry point via de-serialization
+		return this; //default implementation
+	}
+}
