40687fc2dbc3bf57fb2d4f5a1c0019cb6a50b175
mireif <reif@st.informatik.tu-darmstadt.de>
2016-01-27 10:31:06 +0100

    filled the map with some logic

--- src/lib/expressions/Map.java
+++ src/lib/expressions/Map.java
@@ -3,22 +3,90 @@ package expressions;
 /**
  * Created by eichberg on 26.01.16.
  */
-public class Map<K,V> {
+public class Map<K, V> {
 
+    LinkedEntry root;
+    LinkedEntry last;
 
-    public Map(){
+    public Map() {
 
     }
+
 
     public void add(K k, V v) {
+        if (root == null) {
+            root = new LinkedEntry(k, v);
+            last = root;
+        } else {
+            boolean found = false;
 
+            LinkedEntry cur = root;
+            while (cur != null) {
+                if (cur.key.equals(k)) {
+                    cur.value = v;
+                    found = true;
+                }
+                cur = cur.getNextEntry();
+            }
+
+            if (!found) {
+                LinkedEntry newElement = new LinkedEntry(k, v);
+                last.setNextEntry(newElement);
+                last = newElement;
+            }
+        }
     }
 
-    public String toString(){
-     return "Map()";
+    public String toString() {
+        String output = "Map(";
+
+        LinkedEntry cur = root;
+        while (cur != null) {
+            output += cur.toString();
+            if(cur != last)
+                output += ",";
+
+            cur = cur.getNextEntry();
+        }
+
+        return output + ")";
     }
 
     public V get(K name) {
-        return null; // TODO
+
+        LinkedEntry cur = root;
+        while(cur != null){
+            if(cur.key.equals(name))
+                return cur.value;
+
+            cur = cur.getNextEntry();
+        }
+
+        return null;
+    }
+
+    private class LinkedEntry {
+
+        final K key;
+        V value;
+
+        private LinkedEntry nextEntry;
+
+        LinkedEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public LinkedEntry getNextEntry() {
+            return nextEntry;
+        }
+
+        public void setNextEntry(LinkedEntry nextEntry) {
+            this.nextEntry = nextEntry;
+        }
+
+        public String toString(){
+            return key.toString() + " -> " + value.toString();
+        }
     }
 }
