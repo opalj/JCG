94076176d12cf9c9deaecf2408ec2f6f86cb9a2e
Florian Kübler <kuebler@st.informatik.tu-darmstadt.de>
2018-04-24 13:41:00 +0200

    Fixed path to RT.jar

--- jcg_evaluation/src/main/scala/Evaluation.scala
+++ jcg_evaluation/src/main/scala/Evaluation.scala
@@ -1,10 +1,10 @@
 import java.io.File
 
-import lib.annotations.documentation.CGFeature
+import org.opalj.bytecode
 
 object Evaluation {
     def main(args: Array[String]): Unit = {
-        val rtJar = "/Library/Java/JavaVirtualMachines/jdk1.8.0_162.jdk/Contents/Home/jre/lib/rt.jar"
+        val rtJar = bytecode.RTJar.getAbsolutePath
         val jarDir = new File("result/")
         if (jarDir.exists && jarDir.isDirectory) {
             val jars = jarDir.listFiles((_, name) ⇒ name.endsWith(".jar")).sorted
@@ -20,7 +20,6 @@ object Evaluation {
                 for (cgAlgo ← adapter.possibleAlgorithms()) {
                     print(s"${adapter.frameworkName()} $cgAlgo")
                     for (tgt ← jars) {
-                        //print(s"\tproject $tgt\t")
                         adapter.serializeCG(cgAlgo, tgt.getAbsolutePath, rtJar, s"${adapter.frameworkName()}-$cgAlgo-${tgt.getName}.json")
                         System.gc()
                         print(s"\t${CGMatcher.matchCallSites(tgt.getAbsolutePath, s"${adapter.frameworkName()}-$cgAlgo-${tgt.getName}.json")}")
@@ -28,8 +27,6 @@ object Evaluation {
                     println()
                 }
             }
-            // List("RTA", "0-CFA", "1-CFA", "0-1-CFA")
-            // WalaJCGAdapter.main(Array(cgAlgo, tgt.getAbsolutePath, s"wala-$cgAlgo-${tgt.getName}.json"))
         }
     }
 }
