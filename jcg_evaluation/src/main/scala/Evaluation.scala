4f6eb5648695242f1259eeabb4e7a2ad208cc22e
Michael Reif <>
2018-05-15 13:35:52 +0200

    evaluation can now be written to a file or to the console

--- jcg_evaluation/src/main/scala/Evaluation.scala
+++ jcg_evaluation/src/main/scala/Evaluation.scala
@@ -1,33 +1,73 @@
 import java.io.File
+import java.io.BufferedWriter
+import java.io.FileWriter
+import java.io.OutputStreamWriter
+import java.io.Writer
 
 import org.opalj.bytecode
 
 object Evaluation {
+
+    val debug = true;
+    val OUTPUT_FILENAME = "result/evaluation_results.tsv"
+    val EVALUATION_ADAPTERS = List(new SootJCGAdatper(), new WalaJCGAdapter())
+
+
     def main(args: Array[String]): Unit = {
         val rtJar = bytecode.RTJar.getAbsolutePath
         val jarDir = new File("result/")
+
+        var jarFilter = ""
+        var target = ""
+        args.sliding(2, 2).toList.collect {
+            case Array("--output", t: String) => target = t
+            case Array("--filter", name: String) => jarFilter = name
+        }
+
+        val outputTarget = getOutputTarget(target)
+        val ow = new BufferedWriter(outputTarget)
+
         if (jarDir.exists && jarDir.isDirectory) {
-            val jarFilter = if(args.length > 0) args(0) else "";
             val jars = jarDir.listFiles((_, name) ⇒ name.endsWith(".jar")).sorted.filter(_.getName.startsWith(jarFilter))
-            // print header
-            print("algorithm")
-            for (tgt <- jars) {
-                print(s"\t$tgt")
-            }
-            println()
-            // do the hard work
-            for (adapter ← List(new SootJCGAdatper(), new WalaJCGAdapter)) {
-                // soot
+            printHeader(ow, jars)
+            for (adapter ← EVALUATION_ADAPTERS) {
                 for (cgAlgo ← adapter.possibleAlgorithms()) {
-                    print(s"${adapter.frameworkName()} $cgAlgo")
+                    ow.write(s"${adapter.frameworkName()} $cgAlgo")
                     for (tgt ← jars) {
                         adapter.serializeCG(cgAlgo, tgt.getAbsolutePath, rtJar, s"${adapter.frameworkName()}-$cgAlgo-${tgt.getName}.json")
                         System.gc()
-                        print(s"\t${CGMatcher.matchCallSites(tgt.getAbsolutePath, s"${adapter.frameworkName()}-$cgAlgo-${tgt.getName}.json")}")
+                        val result : Boolean = CGMatcher.matchCallSites(tgt.getAbsolutePath, s"${adapter.frameworkName()}-$cgAlgo-${tgt.getName}.json")
+                        ow.write(s"\t$result")
                     }
-                    println()
+                    ow.newLine()
                 }
             }
+            ow.flush()
+            ow.close()
+        }
+    }
+
+    private def printHeader(ow: BufferedWriter, jars: Array[File]) = {
+        ow.write("algorithm")
+        for (tgt <- jars) {
+            ow.write(s"\t$tgt")
+        }
+        ow.newLine()
+    }
+
+    def getOutputTarget(target: String) : Writer = {
+        target match {
+            case "c" => new OutputStreamWriter(System.out)
+            case "f" => {
+                val outputFile = new File(OUTPUT_FILENAME);
+                if(outputFile.exists()){
+                    outputFile.delete()
+                    outputFile.createNewFile()
+                }
+
+                new FileWriter(outputFile, false)
+            }
+            case _ => new OutputStreamWriter(System.out)
         }
     }
 }
