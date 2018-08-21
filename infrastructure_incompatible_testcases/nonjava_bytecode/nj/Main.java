7b0bd3e290ec0fb725d252ab2b8fd12f9f1235ad
Dominik Helm <helm@cs.tu-darmstadt.de>
2018-08-21 14:12:07 +0200

    Fixed annotation in nj testcase

---
+++ infrastructure_incompatible_testcases/nonjava_bytecode/nj/Main.java
@@ -0,0 +1,38 @@
+package nj;
+
+import lib.annotations.callgraph.CallSite;
+
+interface SuperIntf {
+    default void m(){ Helper.println("SuperIntf.m"); };
+}
+
+interface Intf extends SuperIntf {
+    // In Java it is not possible to have a subclass that defines a
+    // method with the same name and signature, but which is static.
+    // This is, however, possible at the bytecode level and the JVM
+    // will call the default method.
+    //static void m(){ Helper.println("Intf.m"); };
+}
+
+interface SubIntf extends Intf {}
+
+class C implements SubIntf {}
+
+class Helper {
+    public static void println(java.lang.String s) {
+        System.out.println(s);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        run(new C());
+    }
+
+    @CallSite(name = "m", line = 35, resolvedTargets = "Lnj/SuperIntf;", prohibitedTargets = "Lnj/Intf;")
+    public static void run(SubIntf c) {
+        // This invokes the default method from SuperIntf
+    	c.m();
+    }
+
+}
