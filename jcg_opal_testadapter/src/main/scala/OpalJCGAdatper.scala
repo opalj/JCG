56d5e0af498e4f6294702635bafb6caf1b385be5
Florian Kübler <kuebler@st.informatik.tu-darmstadt.de>
2019-02-08 13:01:30 +0100

    serialization of call-graph for OPAL now uses buffers

--- jcg_opal_testadapter/src/main/scala/OpalJCGAdatper.scala
+++ jcg_opal_testadapter/src/main/scala/OpalJCGAdatper.scala
@@ -1,19 +1,17 @@
-import java.io.BufferedOutputStream
+import java.io.BufferedWriter
 import java.io.File
-import java.io.FileOutputStream
+import java.io.FileWriter
+import java.io.Writer
 import java.net.URL
 
 import scala.collection.JavaConverters._
-import scala.collection.mutable
 
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 import com.typesafe.config.ConfigValueFactory
-import play.api.libs.json.Json
 
 import org.opalj.fpcf.FinalEP
 import org.opalj.fpcf.PropertyStore
-import org.opalj.br.Code
 import org.opalj.br.DeclaredMethod
 import org.opalj.br.analyses.DeclaredMethods
 import org.opalj.br.analyses.DeclaredMethodsKey
@@ -29,6 +27,7 @@ import org.opalj.br.fpcf.cg.properties.Callees
 import org.opalj.br.fpcf.cg.properties.NoCallees
 import org.opalj.br.fpcf.cg.properties.NoCalleesDueToNotReachableMethod
 import org.opalj.br.instructions.MethodInvocationInstruction
+import org.opalj.br.ObjectType
 import org.opalj.ai.domain.l1.DefaultDomainWithCFGAndDefUse
 import org.opalj.ai.fpcf.properties.AIDomainFactoryKey
 import org.opalj.tac.fpcf.analyses.cg.LazyCalleesAnalysis
@@ -88,10 +87,10 @@ object OpalJCGAdatper extends JCGTestAdapter {
                     "org.opalj.br.analyses.cg.InitialEntryPointsKey.entryPoints",
                     ConfigValueFactory.fromIterable(
                         Seq(
-                            ConfigValueFactory.fromMap(Map(
-                                "declaringClass" → mainClass.replace('.', '/'), "name" → "main"
-                            ).asJava)
-                        ).asJava
+                        ConfigValueFactory.fromMap(Map(
+                            "declaringClass" → mainClass.replace('.', '/'), "name" → "main"
+                        ).asJava)
+                    ).asJava
                     )
                 ).withValue(
                         "org.opalj.br.analyses.cg.InitialInstantiatedTypesKey.analysis",
@@ -165,121 +164,135 @@ object OpalJCGAdatper extends JCGTestAdapter {
 
         val after = System.nanoTime()
 
-        var reachableMethods = Set.empty[ReachableMethod]
-        val methodCache = mutable.Map.empty[Method, Method]
-        val stringCache = mutable.Map.empty[String, String]
-
-        for (
+        val out = new BufferedWriter(new FileWriter(outputFile))
+        out.write(s"""{"reachableMethods":[""")
+        var firstRM = true
+        for {
             dm ← declaredMethods.declaredMethods if (!dm.hasSingleDefinedMethod && !dm.hasMultipleDefinedMethods) ||
                 (dm.hasSingleDefinedMethod && dm.definedMethod.classFile.thisType == dm.declaringClassType)
-        ) {
-            val m = createMethodObject(dm, methodCache, stringCache)
-            ps(dm, Callees.key) match {
-                case FinalEP(_, NoCalleesDueToNotReachableMethod) ⇒
-                case FinalEP(_, NoCallees) ⇒
-                    reachableMethods += ReachableMethod(m, Set.empty)
-                case FinalEP(_, cs: Callees) ⇒
-                    val body = dm.definedMethod.body
-                    val callSites = cs.callSites().flatMap {
-                        case (pc, callees) ⇒
-                            createCallSites(body, pc, callees, methodCache, stringCache)
-                    }.toSet
-                    reachableMethods += ReachableMethod(m, callSites)
+            calleeEOptP = ps(dm, Callees.key)
+            if calleeEOptP.ub ne NoCalleesDueToNotReachableMethod
+        } {
+            if (firstRM) {
+                firstRM = false
+            } else {
+                out.write(",")
             }
+            out.write("{\"method\":")
+            writeMethodObject(dm, out)
+            out.write(",\"callSites\":[")
+            calleeEOptP match {
+                case FinalEP(_, NoCallees) ⇒
+                case FinalEP(_, callees: Callees) ⇒
+                    writeCallSites(dm, callees, out)
+            }
+            out.write("]}")
         }
+        out.write("]}")
+        out.flush()
+        out.close()
 
         ps.shutdown()
-
-        val file = new BufferedOutputStream(new FileOutputStream(outputFile))
-        file.write(Json.toBytes(Json.toJson(ReachableMethods(reachableMethods))))
-        file.flush()
-        file.close()
 
         after - before
     }
 
-    private def createCallSites(
-        bodyO:       Option[Code],
-        pc:          Int,
-        callees:     Iterator[DeclaredMethod],
-        methodCache: mutable.Map[Method, Method],
-        stringCache: mutable.Map[String, String]
-    ): Seq[CallSite] = bodyO match {
-        case None ⇒ callees.map(createIndividualCallSite(_, -1, methodCache, stringCache)).toSeq
-        case Some(body) ⇒
-            val declaredO = body.instructions(pc) match {
-                case MethodInvocationInstruction(dc, _, name, desc) ⇒ Some(dc, name, desc)
-                case _                                              ⇒ None
+    private def writeCallSites(
+        method:  DeclaredMethod,
+        callees: Callees,
+        out:     Writer
+    )(implicit ps: PropertyStore, declaredMethods: DeclaredMethods): Unit = {
+        val bodyO = if (method.hasSingleDefinedMethod) method.definedMethod.body else None
+        var first = true
+        for ((pc, targets) ← callees.callSites()) {
+            bodyO match {
+                case None ⇒
+                    for (tgt ← targets) {
+                        if (first) first = false
+                        else out.write(",")
+                        writeCallSite(tgt, -1, Iterator(tgt), out)
+                    }
+
+                case Some(body) ⇒
+                    val declaredTgtO = body.instructions(pc) match {
+                        case MethodInvocationInstruction(dc, _, name, desc) ⇒ Some(dc, name, desc)
+                        case _                                              ⇒ None
+                    }
+
+                    val line = body.lineNumber(pc).getOrElse(-1)
+
+                    if (declaredTgtO.isDefined) {
+                        val (dc, name, desc) = declaredTgtO.get
+                        val declaredType =
+                            if (dc.isArrayType)
+                                ObjectType.Object
+                            else
+                                dc.asObjectType
+
+                        val declaredTarget = declaredMethods(
+                            declaredType, declaredType.packageName, declaredType, name, desc
+                        )
+
+                        val (directCallees, indirectCallees) = targets.partition { callee ⇒
+                            callee.name == name && // TODO check descriptor correctly for refinement
+                                callee.descriptor.parametersCount == desc.parametersCount
+                        }
+
+                        for (tgt ← indirectCallees) {
+                            if (first) first = false
+                            else out.write(",")
+                            writeCallSite(tgt, line, Iterator(tgt), out)
+                        }
+                        if (directCallees.nonEmpty) {
+                            if (first) first = false
+                            else out.write(",")
+                            writeCallSite(declaredTarget, line, directCallees, out)
+                        }
+
+                    } else {
+                        for (tgt ← targets) {
+                            if (first) first = false
+                            else out.write(",")
+                            writeCallSite(tgt, line, Iterator(tgt), out)
+                        }
+                    }
             }
-
-            val line = body.lineNumber(pc).getOrElse(-1)
-
-            if (declaredO.isDefined) {
-                val (dc, name, desc) = declaredO.get
-                val declaredTarget =
-                    Method(
-                        name,
-                        dc.toJVMTypeName,
-                        desc.returnType.toJVMTypeName,
-                        desc.parameterTypes.map[String](_.toJVMTypeName).toList
-                    )
-
-                val (directCallees, indirectCallees) = callees.partition { callee ⇒
-                    callee.name == name && // TODO check descriptor correctly for refinement
-                        callee.descriptor.parametersCount == desc.parametersCount
-                }
-
-                indirectCallees.map(createIndividualCallSite(_, line, methodCache, stringCache)).toSeq :+
-                    CallSite(
-                        declaredTarget,
-                        line,
-                        directCallees.map(createMethodObject(_, methodCache, stringCache)).toSet
-                    )
-            } else {
-                callees.map(createIndividualCallSite(_, line, methodCache, stringCache)).toSeq
-            }
-    }
-
-    def createIndividualCallSite(
-        method:      DeclaredMethod,
-        line:        Int,
-        methodCache: mutable.Map[Method, Method],
-        stringCache: mutable.Map[String, String]
-    ): CallSite = {
-        CallSite(
-            createMethodObject(method, methodCache, stringCache),
-            line,
-            Set(createMethodObject(method, methodCache, stringCache))
-        )
-    }
-
-    private def createMethodObject(
-        method:      DeclaredMethod,
-        methodCache: mutable.Map[Method, Method],
-        stringCache: mutable.Map[String, String]
-    ): Method = {
-        val m = Method(
-            uniqueString(method.name, stringCache),
-            uniqueString(method.declaringClassType.toJVMTypeName, stringCache),
-            uniqueString(method.descriptor.returnType.toJVMTypeName, stringCache),
-            method.descriptor.parameterTypes.map[String] {
-                pt ⇒ uniqueString(pt.toJVMTypeName, stringCache)
-            }.toList
-        )
-        if (methodCache.contains(m)) {
-            methodCache(m)
-        } else {
-            methodCache += (m → m)
-            m
         }
     }
 
-    private def uniqueString(s: String, stringCache: mutable.Map[String, String]): String = {
-        if (stringCache.contains(s))
-            stringCache(s)
-        else {
-            stringCache += (s → s)
-            s
+    private def writeCallSite(
+        declaredTarget: DeclaredMethod,
+        line:           Int,
+        targets:        Iterator[DeclaredMethod],
+        out:            Writer
+    ): Unit = {
+        out.write("{\"declaredTarget\":")
+        writeMethodObject(declaredTarget, out)
+        out.write(",\"line\":")
+        out.write(line.toString)
+        out.write(",\"targets\":[")
+        var first = true
+        for (tgt ← targets) {
+            if (first) first = false
+            else out.write(",")
+            writeMethodObject(tgt, out)
         }
+        out.write("]}")
+    }
+
+    private def writeMethodObject(
+        method: DeclaredMethod,
+        out:    Writer
+    ): Unit = {
+        out.write("{\"name\":\"")
+        out.write(method.name)
+        out.write("\",\"declaringClass\":\"")
+        out.write(method.declaringClassType.toJVMTypeName)
+        out.write("\",\"returnType\":\"")
+        out.write(method.descriptor.returnType.toJVMTypeName)
+        out.write("\",\"parameterTypes\":[")
+        if (method.descriptor.parametersCount > 0)
+            out.write(method.descriptor.parameterTypes.iterator.map[String](_.toJVMTypeName).mkString("\"", "\",\"", "\""))
+        out.write("]}")
     }
 }
