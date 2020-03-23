4555cf706dce41a1342175ccc550f9e1931d834e
Michael Reif <mi.reif.mr@googlemail.com>
2020-03-23 14:51:30 +0100

    updated compare cg

--- jcg_evaluation/src/main/scala/CompareCGs.scala
+++ jcg_evaluation/src/main/scala/CompareCGs.scala
@@ -1,6 +1,13 @@
 import java.io.FileInputStream
+import java.util
 
 import play.api.libs.json.Json
+
+import scala.collection.mutable
+import scala.collection.JavaConverters._
+import java.util.{HashSet => JHashSet}
+
+
 
 /**
  * @author Dominik Helm
@@ -21,6 +28,8 @@ object CompareCGs {
         var maxFindings = Int.MaxValue
 
         var inPackage = ""
+
+
 
         args.sliding(2, 2).toList.collect {
             case Array("--input1", cg) ⇒
@@ -56,13 +65,21 @@ object CompareCGs {
             println(additionalReachableMethods2.mkString(" ##### Additional Methods - Input 2 #####\n\n\t", "\n\t", "\n\n"))
         }
 
-        val commonReachableMethods = if (showCommon || showBoundaries) {
-            cg1.filter(m ⇒ cg2.contains(m._1)).keySet
-        } else
-            Set.empty[Method]
+        val commonReachableMethods : java.util.HashSet[Method] = if (showCommon || showBoundaries) {
+            val cg1Keys = cg1.keySet.iterator
+            val cg2Keys = cg2.keySet
+            val common = new java.util.HashSet[Method]()
+            while(cg1Keys.hasNext) {
+                val m1 = cg1Keys.next()
+                if(cg2.contains(m1))
+                    common add m1
+            }
+            common
+        } else new util.HashSet[Method]()
 
         if (showCommon) {
-            println(commonReachableMethods.toSeq.sortBy(_.declaringClass).take(maxFindings).mkString(" ##### Common Methods #####\n\n\t", "\n\t", "\n\n"))
+            val seq = commonReachableMethods.asScala.toSeq
+            println(seq.sortBy(_.declaringClass).take(maxFindings).mkString(" ##### Common Methods #####\n\n\t", "\n\t", "\n\n"))
         }
 
         if (showReachable) {
@@ -75,8 +92,8 @@ object CompareCGs {
         }
 
         if (showBoundaries) {
-            val boundaries1 = extractBoundaries(cg1, commonReachableMethods, inPackage).toSeq.sortBy(_._1.declaringClass).take(maxFindings)
-            val boundaries2 = extractBoundaries(cg2, commonReachableMethods, inPackage).toSeq.sortBy(_._1.declaringClass).take(maxFindings)
+            val boundaries1 = extractBoundaries(cg1, commonReachableMethods, inPackage).asScala.toSeq.sortBy(_.m.declaringClass).take(maxFindings)
+            val boundaries2 = extractBoundaries(cg2, commonReachableMethods, inPackage).asScala.toSeq.sortBy(_.m.declaringClass).take(maxFindings)
 
             println(boundaries1.mkString(" ##### Boundary Methods - Input 1 #####\n\n\t", "\n\t", "\n\n"))
             println(boundaries2.mkString(" ##### Boundary Methods - Input 2 #####\n\n\t", "\n\t", "\n\n"))
@@ -86,35 +103,61 @@ object CompareCGs {
     private def extractAdditionalMethods(
         baseCG: Map[Method, Set[CallSite]], comparedTo: Map[Method, Set[CallSite]]
     ): Set[Method] = {
-        baseCG.filter(m ⇒ !comparedTo.contains(m._1)).keySet
+        baseCG.keySet.filter(!comparedTo.contains(_))
+//        baseCG.filter(m ⇒ !comparedTo.contains(m.._1)).keySet
     }
 
     private def extractReachableApplicationMethods(
         appPackages: List[String], cg: Map[Method, Set[CallSite]]
     ): Set[Method] = {
-        cg.filter(rm ⇒ appPackages.exists(p ⇒ rm._1.declaringClass.startsWith(s"L$p/"))).keySet
+        cg.filter(rm ⇒ appPackages.iterator.exists(p ⇒ rm._1.declaringClass.startsWith(s"L$p/"))).keySet
     }
+
+    case class MethodBoundary(m: Method, target: String)
 
     private def extractBoundaries(
-        cg: Map[Method, Set[CallSite]], commonReachableMethods: Set[Method], inPackage: String
-    ): Set[(Method, String)] = {
-        commonReachableMethods.filter { caller ⇒
-            if (caller.declaringClass.startsWith(inPackage)) {
+        cg: Map[Method, Set[CallSite]], commonReachableMethods: JHashSet[Method], inPackage: String
+    ): JHashSet[MethodBoundary] = {
+        val boundaries = new JHashSet[MethodBoundary]()
+
+        val itr = commonReachableMethods.iterator()
+        while(itr.hasNext) {
+            val caller = itr.next()
+            if(caller.declaringClass.startsWith(inPackage)) {
                 val callees = cg(caller).flatMap(_.targets)
-                callees.exists(target ⇒ !commonReachableMethods.contains(target))
-            } else {
-                false
+                if(callees.iterator.exists(!commonReachableMethods.contains(_))){
+                    val differences = new StringBuilder("\n\t\t")
+                    callees.iterator.foreach { callee =>
+                        if(!commonReachableMethods.contains(callee)) {
+                            differences.append(s"${transitiveHull(callee, cg, commonReachableMethods)}: $callee\n\t\t")
+                        }
+                    }
+                    boundaries.add(MethodBoundary(caller, differences.result()))
+                }
             }
-        }.map { caller ⇒
-            val callees = cg(caller).flatMap(_.targets)
-            (caller, callees.diff(commonReachableMethods).map(m ⇒ s"${transitiveHull(m, cg, commonReachableMethods)}: $m").mkString("\n\t\t", "\n\t\t", ""))
         }
+
+        boundaries
+//        commonReachableMethods.filter { caller ⇒
+//            if (caller.declaringClass.startsWith(inPackage)) {
+//                val callees = cg(caller).flatMap(_.targets)
+//                callees.iterator.exists(target ⇒ !commonReachableMethods.contains(target))
+//            } else {
+//                false
+//            }
+//        }.map { caller ⇒
+//            val callees = cg(caller).flatMap(_.targets)
+//            (caller, callees.diff(commonReachableMethods).map(m ⇒ s"${transitiveHull(m, cg, commonReachableMethods)}: $m").mkString("\n\t\t", "\n\t\t", ""))
+//        }
     }
 
-    private def transitiveHull(method: Method, cg: Map[Method, Set[CallSite]], commonReachableMethods: Set[Method]): (Int, Int) = {
-        var reachableMethods: Set[Method] = Set(method)
-        var nonCommon: Set[Method] = Set(method)
-        var worklist: List[Method] = List(method)
+    private def transitiveHull(method: Method, cg: Map[Method, Set[CallSite]], commonReachableMethods: JHashSet[Method]): (Int, Int) = {
+        val reachableMethods = new java.util.HashSet[Method]()
+        reachableMethods add method
+        val nonCommon = new java.util.HashSet[Method]()
+        nonCommon add method
+
+        var worklist: mutable.Queue[Method] = mutable.Queue(method)
 
         while (worklist.nonEmpty) {
             val currentMethod = worklist.head
@@ -125,9 +168,9 @@ object CompareCGs {
             } {
                 if (!reachableMethods.contains(t)) {
                     if (!commonReachableMethods.contains(t))
-                        nonCommon += t
-                    reachableMethods += t
-                    worklist ::= t
+                        nonCommon add t
+                    reachableMethods add t
+                    worklist enqueue t
                 }
             }
         }
