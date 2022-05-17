634739bdf50780132732a098a6c0039a9352a4cb
tobias roth <roth@st.informatik.tu-darmstadt.de>
2022-05-17 15:46:00 +0200

    Added Timeout state

--- jcg_annotation_matcher/src/main/scala/Assessment.scala
+++ jcg_annotation_matcher/src/main/scala/Assessment.scala
@@ -6,14 +6,16 @@
 trait Assessment {
     def isSound : Boolean
     def isUnsound: Boolean
+    def isTimeout: Boolean
     def combine(other: Assessment) : Assessment
-    override def toString: String
+    def toString: String
     def shortNotation : String
 }
 
 object Sound extends Assessment {
     override def isSound: Boolean = true
     override def isUnsound: Boolean = false
+    override def isTimeout: Boolean = false
     override def combine(other: Assessment): Assessment = other
     override def toString: String = "Sound"
     override def shortNotation: String = "S"
@@ -22,9 +24,10 @@ object Sound extends Assessment {
 object Imprecise extends Assessment {
     override def isSound: Boolean = false
     override def isUnsound: Boolean = false
+    override def isTimeout: Boolean = false
     override def combine(other: Assessment): Assessment = {
         other match {
-            case Unsound => other
+            case Unsound | Error | Timeout => other
             case _ => Imprecise
         }
     }
@@ -35,7 +38,13 @@ object Imprecise extends Assessment {
 object Unsound extends Assessment {
     override def isSound: Boolean = false
     override def isUnsound: Boolean = true
-    override def combine(other: Assessment): Assessment = Unsound
+    override def isTimeout: Boolean = false
+    override def combine(other: Assessment): Assessment = {
+      other match {
+          case Error | Timeout => other
+          case  _ => Unsound
+      }
+    }
     override def toString: String = "Unsound"
     override def shortNotation: String = "U"
 }
@@ -43,7 +52,22 @@ object Unsound extends Assessment {
 object Error extends Assessment {
     override def isSound: Boolean = false
     override def isUnsound: Boolean = false
-    override def combine(other: Assessment): Assessment = Error
+    override def isTimeout: Boolean = false
+    override def combine(other: Assessment): Assessment = {
+        other match {
+            case Timeout => other
+            case _ => Error
+        }
+    }
     override def toString: String = "Error"
     override def shortNotation: String = "E"
+}
+
+object Timeout extends Assessment {
+    def isSound : Boolean = false
+    def isUnsound: Boolean = false
+    def isTimeout: Boolean = true
+    def combine(other: Assessment) : Assessment = Timeout
+    override def toString: String = "Timeout"
+    def shortNotation : String = "T"
 }
