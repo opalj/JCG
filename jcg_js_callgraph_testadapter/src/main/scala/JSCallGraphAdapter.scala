d86138d5abe068c3cb75312c28b73b9800b4e610
Nico Kunz <nicokunznk00@gmail.com>
2024-08-08 01:40:04 +0200

    Refactor JSCallGraphAdapter

--- jcg_js_callgraph_testadapter/src/main/scala/JSCallGraphAdapter.scala
+++ jcg_js_callgraph_testadapter/src/main/scala/JSCallGraphAdapter.scala
@@ -5,42 +5,59 @@ object JSCallGraphAdapter extends TestAdapter {
 
     val possibleAlgorithms: Array[String] = Array("NONE", "ONESHOT", "DEMAND", "FULL")
     val frameworkName: String = "js-callgraph"
+    private val command = "js-callgraph"
 
     def main(args: Array[String]): Unit = {
         // generate call graphs for all algorithms
+        serializeAllCGs("testcasesOutput/js", s"results/js/$frameworkName")
+    }
+
+    def serializeAllCGs(inputDirPath: String, outputDirPath: String): Unit = {
         for (algo <- possibleAlgorithms) {
-            generateCallGraphs(s"results/js/$frameworkName", algo)
+            generateCallGraphs(inputDirPath, outputDirPath, algo)
         }
     }
 
-    private def generateCallGraphs(outputDirPath: String, algorithm: String): Unit = {
+    /**
+     * Generates all call graphs with the given algorithm.
+     *
+     * @param outputDirPath The directory to write the call graphs to.
+     * @param algorithm     The algorithm to use for generating the call graphs.
+     */
+    private def generateCallGraphs(inputDirPath: String, outputDirPath: String, algorithm: String): Unit = {
         val outputDir = new File(s"$outputDirPath/$algorithm")
         outputDir.mkdirs()
-        val inputDirs = new File("testcasesOutput/js").list()
+        val testDirs = new File(inputDirPath).list()
 
         // check if js-callgraph command is available
-        val command = "js-callgraph"
         try {
             sys.process.Process(Seq(command, "-h")).!!
         } catch {
-            case e: Exception => {
-                println(s"[Error]: $command not found")
-            }
+            case e: Exception => println(s"[Error]: $command not found")
         }
 
         // generate callgraph for every testcase
-        inputDirs.foreach(inputDir => {
-            val inputDirPath = s"testcasesOutput/js/$inputDir"
-            val outputPath = s"${outputDir.getAbsolutePath}/$inputDir.json"
-            val args = Seq("--cg", inputDirPath, "--output", outputPath, "--strategy", algorithm)
-            if (debug) println(s"[DEBUG] executing ${(Seq(command) ++ args).mkString(" ")}")
-            try {
-                sys.process.Process(Seq(command) ++ args).!!
-            } catch {
-                case e: Exception => println(s"[Error]: $command failed for $inputDir")
-            }
+        testDirs.foreach(testDir => {
+            serializeCG(algorithm, outputDir.getAbsolutePath, s"$inputDirPath/$testDir")
         })
 
         println("Call graphs generated!")
+    }
+
+
+    def serializeCG(algorithm: String, outputDirPath: String, inputDirPath: String): Long = {
+        val outputPath = s"$outputDirPath/${inputDirPath.split(File.separator).last}.json"
+        val args = Seq("--cg", inputDirPath, "--output", outputPath, "--strategy", algorithm)
+        if (debug) println(s"[DEBUG] executing ${(Seq(command) ++ args).mkString(" ")}")
+
+        val start = System.nanoTime()
+        try {
+            sys.process.Process(Seq(command) ++ args).!!
+        } catch {
+            case e: Exception => println(s"[Error]: $command failed for $inputDirPath")
+        }
+        val end = System.nanoTime()
+        if (debug) println(s"Call graph for $inputDirPath generated in ${end - start} ns")
+        end - start
     }
 }
