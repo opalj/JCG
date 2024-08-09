3056cdcd92969813accdfb617bdb08736ae623fd
Nico Kunz <nicokunznk00@gmail.com>
2024-08-10 01:48:04 +0200

    Refactor Code2flowCallGraphAdapter to extend JSTestAdapter

--- jcg_code2flow_testadapter/src/main/scala/Code2flowCallGraphAdapter.scala
+++ jcg_code2flow_testadapter/src/main/scala/Code2flowCallGraphAdapter.scala
@@ -1,71 +1,103 @@
 import upickle.default._
 
-import java.io.{BufferedWriter, File, FileWriter}
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
 import scala.collection.mutable
 
-object Code2flowCallGraphAdapter extends TestAdapter {
+object Code2flowCallGraphAdapter extends JSTestAdapter {
     val debug: Boolean = false
 
     val possibleAlgorithms: Array[String] = Array("NONE")
     val frameworkName: String = "code2flow"
+    private val command = "code2flow"
 
-    def main(args: Array[String]): Unit = {
-        // generate call graphs for all algorithms
+    def main(args: Array[String]): Unit = serializeAllCGs("testcasesOutput/js", s"results/js/$frameworkName")
+
+    /**
+     * Generates all call graphs with the given algorithm.
+     *
+     * @param inputDirPath  The directory containing the input files to generate call graphs for.
+     * @param outputDirPath The directory to write the call graphs to.
+     */
+    override def serializeAllCGs(inputDirPath: String, outputDirPath: String): Unit = {
         for (algo <- possibleAlgorithms) {
-            generateCallGraphs(s"results/js/$frameworkName", algo)
+            generateCallGraphs(inputDirPath, outputDirPath, algo)
         }
     }
 
-    private def generateCallGraphs(outputDirPath: String, algorithm: String): Unit = {
+    private def generateCallGraphs(inputDirPath: String, outputDirPath: String, algorithm: String): Unit = {
         val outputDir = new File(s"$outputDirPath/$algorithm")
         outputDir.mkdirs()
-        val inputDirs = new File("testcasesOutput/js").list()
+        val testDirs = new File(inputDirPath).list()
 
         // check if js-callgraph command is available
         val command = "code2flow"
         try {
             sys.process.Process(Seq(command, "--help")).!!
         } catch {
-            case e: Exception => {
+            case e: Exception =>
                 println(s"${Console.RED}[Error]: $command command not found, make sure it is installed and in your PATH${Console.RESET}")
                 return
-            }
+
         }
 
         // generate callgraph for every testcase
-        for (inputDir <- inputDirs) {
-            val inputDirPath = s"testcasesOutput/js/$inputDir"
-            val outputPath = s"${outputDir.getAbsolutePath}/$inputDir.json"
-            val args = Seq(inputDirPath, "--output", outputPath, "-q", "--source-type=module", "--no-trimming")
-            println(s"[DEBUG] executing ${(Seq(command) ++ args).mkString(" ")}")
-            val processSucceeded = try {
-                sys.process.Process(Seq(command) ++ args).!!
-                true
-            } catch {
-                case e: Exception =>
-                    println(s"${Console.RED}[Error]: $inputDir failed to generate${Console.RESET}")
-                    false
-            }
-
-            if (processSucceeded) {
-                val outputFile = new File(outputPath)
-                try {
-                    val json = toCommonFormat(outputFile)
-                    val bw = new BufferedWriter(new FileWriter(outputFile))
-                    bw.write(json)
-                    bw.close()
-                } catch {
-                    case e: Exception =>
-                        println(s"${Console.RED}[Error]: Failed to process and write the call graph for $inputDir${Console.RESET}")
-                }
-            }
-        }
+        testDirs.foreach(testDir => {
+            serializeCG(algorithm, outputDir.getAbsolutePath, s"$inputDirPath/$testDir")
+        })
 
         println("Call graphs generated!")
     }
 
+    /**
+     * Generates a single call graph.
+     *
+     * @param algorithm     The algorithm to use for generating the call graph.
+     * @param outputDirPath The directory to write the call graph to.
+     * @param inputDirPath  The directory containing the input files to generate a call graph for.
+     * @return The time taken to generate the call graph in nanoseconds.
+     */
+    override def serializeCG(algorithm: String, outputDirPath: String, inputDirPath: String): Long = {
+        val outputPath = s"$outputDirPath/${inputDirPath.split(File.separator).last}.json"
+        val args = Seq(inputDirPath, "--output", outputPath, "-q", "--source-type=module", "--no-trimming")
+        if (debug) println(s"[DEBUG] executing ${(Seq(command) ++ args).mkString(" ")}")
+
+        // Generate call graph
+        val start = System.nanoTime()
+        val processSucceeded = try {
+            sys.process.Process(Seq(command) ++ args).!!
+            true
+        } catch {
+            case e: Exception =>
+                println(s"${Console.RED}[Error]: $inputDirPath failed to generate${Console.RESET}")
+                false
+        }
+        val end = System.nanoTime()
+
+        // Process output and convert to common call graph format
+        if (processSucceeded) {
+            val outputFile = new File(outputPath)
+            try {
+                val json = toCommonFormat(outputFile)
+                val bw = new BufferedWriter(new FileWriter(outputFile))
+                bw.write(json)
+                bw.close()
+            } catch {
+                case e: Exception => println(s"${Console.RED}[Error]: Failed to process and write the call graph for $inputDirPath${Console.RESET}")
+            }
+        }
+
+        end - start
+    }
+
+    /**
+     * Converts the code2flow call graph output to the common call graph format.
+     *
+     * @param cgFile The code2flow call graph json file.
+     * @return The call graph in the common format.
+     */
     private def toCommonFormat(cgFile: File): String = {
-        //TODO: Check if file empty
         val cg = ujson.read(cgFile).obj("graph")
         val nodes: mutable.Map[String, Node] = cg("nodes").obj.map(node => getNodeFromKV(node, cgFile.getAbsolutePath))
         val edges: Array[Edge] = cg("edges").arr.map(edge => Edge(nodes(edge("source").str), nodes(edge("target").str))).toArray
