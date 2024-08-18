b7e756f146d5f268751adceb5432c16462c8a8b9
Nico Kunz <nicokunznk00@gmail.com>
2024-08-19 00:37:30 +0200

    Refactor TestAdapter and JavaTestAdapter

--- jcg_testadapter_commons/src/main/scala/JavaTestAdapter.scala
+++ jcg_testadapter_commons/src/main/scala/JavaTestAdapter.scala
@@ -1,18 +1,15 @@
 trait JavaTestAdapter extends TestAdapter {
-    val language = "java"
-    /**
-     * Constructs a call graph for the given target and saves the serialized version
-     * ({@link ReachableMethods}) into the specified output file and return the elapsed time in
-     * nanoseconds
-     *
-     * @param mainClass the main-class to be analysed (x.y.Foo) or `null` in case of a library
-     * @return the elapsed nanoseconds
-     */
-    def serializeCG(algorithm: String,
-                    target: String,
-                    mainClass: String,
-                    classPath: Array[String],
-                    JDKPath: String,
-                    analyzeJDK: Boolean,
-                    outputFile: String): Long
+    override val language: String = "java"
+
+    // Java-specific serializeCG
+    def serializeCG(algorithm: String, target: String, mainClass: String, classPath: Array[String], JDKPath: String, analyzeJDK: Boolean, outputFile: String): Long
+
+    // General serializeCG, overriding the base one
+    override def serializeCG(algorithm: String, outputDirPath: String, inputDirPath: String): Long = {
+        0
+    }
+
+    // Override serializeAllCGs if necessary
+    override def serializeAllCGs(inputDirPath: String, outputDirPath: String): Unit = {
+    }
 }
--- jcg_testadapter_commons/src/main/scala/TestAdapter.scala
+++ jcg_testadapter_commons/src/main/scala/TestAdapter.scala
@@ -3,4 +3,8 @@ trait TestAdapter {
     val language: String
 
     def possibleAlgorithms: Array[String]
+
+    def serializeCG(algorithm: String, outputDirPath: String, inputDirPath: String): Long
+
+    def serializeAllCGs(inputDirPath: String, outputDirPath: String): Unit
 }
