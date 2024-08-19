c5f67504643d973b1a8b9cd8e344024bdb81e913
Nico Kunz <nicokunznk00@gmail.com>
2024-08-19 02:00:02 +0200

    Refactor JavaTestAdapter

--- jcg_testadapter_commons/src/main/scala/JavaTestAdapter.scala
+++ jcg_testadapter_commons/src/main/scala/JavaTestAdapter.scala
@@ -1,15 +1,14 @@
 trait JavaTestAdapter extends TestAdapter {
     override val language: String = "java"
 
-    // Java-specific serializeCG
+    // Overload serializeCG for java
     def serializeCG(algorithm: String, target: String, mainClass: String, classPath: Array[String], JDKPath: String, analyzeJDK: Boolean, outputFile: String): Long
 
-    // General serializeCG, overriding the base one
+    // Override base methods
     override def serializeCG(algorithm: String, outputDirPath: String, inputDirPath: String): Long = {
-        0
+        0L
     }
 
-    // Override serializeAllCGs if necessary
     override def serializeAllCGs(inputDirPath: String, outputDirPath: String): Unit = {
     }
 }
