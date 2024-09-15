d44b20eb7db1c8f0dc3dc0ccc83698eff55d4751
Nico Kunz <nicokunznk00@gmail.com>
2024-09-16 00:40:20 +0200

    Add parsing of indirectLinks to ExpectedCG

--- jcg_evaluation/src/main/scala/ExpectedCG.scala
+++ jcg_evaluation/src/main/scala/ExpectedCG.scala
@@ -4,7 +4,7 @@ class ExpectedCG(val jsonFile: File) extends CallGraph {
     private val json = parseFileToJson(jsonFile).getOrElse(ujson.Null)
     val filePath: String = jsonFile.getAbsolutePath
     val links: Seq[Seq[String]] = parseLinks("directLinks")
-    val indirectLinks: Seq[Seq[String]] = parseLinks("indirectLinks")
+    val indirectLinks: Seq[String] = parseLinks("indirectLinks").flatMap(_.headOption)
 
     /**
      * Parses the links from the given JSON file.
@@ -15,9 +15,16 @@ class ExpectedCG(val jsonFile: File) extends CallGraph {
     private def parseLinks(linkType: String): Seq[Seq[String]] = {
         if (json.isNull) Seq.empty
         else {
-            json(linkType).arrOpt match {
-                case Some(links) => links.map(_.arr.map(_.str.trim).toSeq).toSeq
-                case None => Seq.empty
+            json(linkType) match {
+                case ujson.Arr(links) =>
+                    // Handle the case where the value is an array of arrays or strings
+                    links.map {
+                        case ujson.Arr(arr) => arr.map(_.str.trim).toSeq
+                        case ujson.Str(str) => Seq(str.trim)
+                        case _              => Seq.empty
+                    }.toSeq
+                case ujson.Str(str) => Seq(Seq(str.trim))
+                case _              => Seq.empty
             }
         }
     }
